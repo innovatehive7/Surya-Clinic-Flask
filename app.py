77eec89a98f6d406be513f8600a0d06908aecdc8
innovatehive7 <innovatehive7@gmail.com>
2025-05-01 11:55:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def static_files(path):
     return "File not found", 404
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=5000)  # Only accessible locally
